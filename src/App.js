3a5cb994c358036139043087f6ae2da1a10c405a
chensong zhu <mzhu@deputy.com>
2017-12-17 21:16:40 +1100

    create dev branch

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+     // TODO
     showSearchPage: false
   }
 
